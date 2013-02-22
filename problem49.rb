15c5b417000c04c1d2b60017c06c37a1dd3bb830
John Yu <johnyu2@gmail.com>
2013-02-22 13:25:19 -0500

    solution to 49

--- problem49.rb
+++ problem49.rb
@@ -12,27 +12,22 @@ def sieve_upto(n)
   s.compact
 end
 
-def is_permutation_prime?(n)
-  count = 0
-  unless n.to_s.index(/0/)
-    n.to_s.split(//).permutation.each do |arr|
-      if arr.join.to_i.prime? && (arr.join.to_f / 1000 >= 1)
-        #p number.join.to_i
-        p count
-        count += 1
-        return true if count == 3
-      end
-    end
-  end
-
-  false
+def are_permutations?(i,j,k)
+	i.to_s.split(//).sort == j.to_s.split(//).sort && i.to_s.split(//).sort == k.to_s.split(//).sort
 end
 
 primes = sieve_upto(10000)
 primes.select!{|n| n.to_s.size == 4 }
 
-primes.each do |prime|
-  if is_permutation_prime?(prime)
-    #p prime
-  end
+primes.each do |i|
+	primes.each do |j|
+		k = j + j - i
+		if k.prime? && k != j && k < 10000
+			if are_permutations?(i,j,k)
+				solution = [i,j,k].sort.join.to_i
+				p solution
+				break
+			end
+		end
+    end
 end
