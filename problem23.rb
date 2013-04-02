ae5c11278a3ba5a4a82b94b8307baa13182602fa
John Yu <johnyu2@gmail.com>
2013-04-02 10:59:01 -0400

    solution to problem 23

--- problem23.rb
+++ problem23.rb
@@ -1,3 +1,7 @@
+#real	0m34.667s
+#user	0m34.368s
+#sys	0m0.086s
+
 require 'prime'
 
 def sum(n)
@@ -15,20 +19,15 @@ end
 #find all abundant numbers, eliminate their multiples up to 28123
 #use a similar strategy as finding all primes, by using a sieve, then sum
 
+MAX = 28123
+abundant_arr = 2.upto(MAX).select { |i| sum(i) > i }.compact
 
-def abundant_sieve_upto(n)
-  s = (0..n).to_a
-  s[0..11] = nil
-  s.each do |p|
-    next unless p
-    #break if p * p > n
-	if sum(p) > p #abundant
-		(p*p).step(n, p) { |m| s[m] = nil }
-	else
-		s[p] = nil
+s = [*0..MAX]
+
+abundant_arr.each do |i|
+	abundant_arr.each do |j|
+		(i + j).step(MAX, j) { |m| s[m] = nil }
 	end
-  end
-  s.compact
 end
 
-p abundant_sieve_upto(25)
+p s.compact.inject(:+)
